2bf38cbaffb0e336fc6594ecacf8e063a370e587
Gaurav <g.jain86078@gmail.com>
2021-03-10 00:37:25 +0530

    Create query.py

---
+++ query.py
@@ -0,0 +1,4 @@
+#Enter your query and no of videos you want data for.
+
+query = 'gate 2020'           #enter your query for which you want to scrap videos
+no_of_videos = 501            #enter number of videos you want to scrap.
